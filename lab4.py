bcea8f460f989fcd6b1859f742a8241a19587028
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-11-21 00:28:11 +0530

    added programs

---
+++ lab4.py
@@ -0,0 +1,21 @@
+# first program
+import pandas as pd
+import matplotlib.pyplot as plt
+data=pd.read_csv('L:\Python\DataVisualization\Resort.csv')
+df=pd.DataFrame(data)
+df.plot(kind='box',x='Year',title="Compare Resorts")
+plt.xlabel('Resorts')
+plt.ylabel('Rating')
+plt.show()
+
+# second program
+
+import pandas as pd
+import matplotlib.pyplot as plt
+data=pd.read_csv('L:\Python\DataVisualization\Resort.csv')
+df=pd.DataFrame(data)
+df.plot(kind='box',x='Year',title="Compare
+Resorts",vert=False,patch_artist=True)
+plt.xlabel('Resorts')
+plt.ylabel('Rating')
+plt.show()
